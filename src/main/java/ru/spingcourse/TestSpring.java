d9e90646c3ca64c68502f7b2771c0fab6ac018ed
Anastasiya_Moon <alleksa_anastasiya@mail.ru>
2020-02-14 00:33:32 +0300

    Внедрение зависимостей. Dependency Injection (DI) (с помощью конструктора)

--- src/main/java/ru/spingcourse/TestSpring.java
+++ src/main/java/ru/spingcourse/TestSpring.java
@@ -8,9 +8,10 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("musicBean", Music.class);
+//        Music music = context.getBean("musicBean", Music.class);
         //внедрение зависимости вручную - в конструктор (жанр музыки)
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
         context.close();
     }
